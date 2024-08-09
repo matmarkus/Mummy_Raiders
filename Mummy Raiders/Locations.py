1e06790282dc122f0eac98f9937163a583db702e
matmarkus <matsmolka@gmail.com>
2024-08-09 21:48:53 +0200

    2400809.21 problems with diffuclty, next step

--- Mummy Raiders/Locations.py
+++ Mummy Raiders/Locations.py
@@ -1,11 +1,13 @@
 locations = [
     {
+        "ID": 0o1,
         "Name": "Ancient Temple",
         "Description": "A sunken temple full of puzzles and traps.",
         "Excavation Time (hours)": 6,
         "Used": False,
     },
     {
+        "ID": 0o2,
         "Name": "Lost City",
         "Description": "Ruins of a forgotten city buried deep in the jungle.",
         "Excavation Time (hours)": 8,
@@ -13,10 +15,3 @@ locations = [
     }
 ]
 
-
-for location in locations:
-    print(f"Location: {location['Name']}")
-    print(f"Description: {location['Description']}")
-    print(f"Time required: {location['Excavation Time (hours)']} hours")
-    print(f"Already used: {'Yes' if location['Used'] else 'No'}")
-    print("-" * 40)
---
+++ Mummy Raiders/TEMP.py
@@ -0,0 +1,5 @@
+import random
+from Locations import locations
+
+random_location = random.choice(locations)
+print(random_location)
